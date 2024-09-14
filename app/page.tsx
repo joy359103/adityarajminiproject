477c6fbc2efc651ebaa376195e0dcef26108e1d7
joy359103 <aditya.bhamidipati2003@gmail.com>
2024-09-14 12:21:55 +0530

    Update page.tsx

--- app/page.tsx
+++ app/page.tsx
@@ -36,11 +36,11 @@ export default async function Index() {
       <Container className={styles.section}>
         <div className={styles.heroInfo}>
           <h1 className={styles.heroTitle}>
-            Kickstart your collaborative&nbsp;app
+            try our collaborative app done by-&nbsp;
           </h1>
           <p className={styles.heroLead}>
-            Use the Liveblocks Starter Kit to build your document-based
-            collaborative app in&nbsp;minutes.
+            21eg109b49'M.Tejaswi Raj Madari 
+            21eg109b37 Aditya &nbsp;
           </p>
         </div>
         <div className={styles.heroActions}>
